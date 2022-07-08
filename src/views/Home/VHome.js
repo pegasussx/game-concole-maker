e2ddd6ce678f65d8573a118c7e44ba840606d542
pegasussx <xiangwenxnwn@gmail.com>
2022-07-09 00:21:47 +0900

    no-cors

--- src/views/Home/VHome.js
+++ src/views/Home/VHome.js
@@ -261,6 +261,7 @@ const VHome = () => {
           'Content-Type': 'application/json;charset=UTF-8',
           'Authorization': 'Bearer 7qcul86v2eqvursm9a60ecgk8g9ofz5m',
           "Access-Control-Allow-Origin": '*',
+          "mode": "no-cors",
           "Access-Control-Allow-Credentials": true,
           "Access-Control-Allow-Methods": "OPTIONS, GET, POST",
           "Access-Control-Allow-Headers": "Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control"
